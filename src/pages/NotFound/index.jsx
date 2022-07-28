f69b514c8509cc5afc7aa40a8274a004e6e0e7de
achmadrizky486 <achmadrizky486@gmail.com>
2022-07-28 23:50:33 +0700

    fix : css

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="h-screen w-screen bg-blue-300 absolute top-0 left-0">NotFound</div>
+  );
+};
+
+export default NotFound;
