ead27e7c068a392ee0634c7586f016eaedbcc84b
achmadrizky486 <achmadrizky486@gmail.com>
2022-07-28 23:51:13 +0700

    feat : add svg

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,4 +1,21 @@
 import RegisterBg from "./image/bg.jpg";
 import LoginBg from "./image/LoginBg.jpg";
+import Facebook from "./svg/Facebook.svg";
+import Discord from "./svg/Discord.svg";
+import Github from "./svg/Github.svg";
+import Instagram from "./svg/Instagram.svg";
+import Telegram from "./svg/Telegram.svg";
+import Twitter from "./svg/Twitter.svg";
+import Logo from "./svg/Logo.svg";
 
-export { RegisterBg, LoginBg };
+export {
+  RegisterBg,
+  LoginBg,
+  Facebook,
+  Github,
+  Instagram,
+  Discord,
+  Logo,
+  Telegram,
+  Twitter,
+};
