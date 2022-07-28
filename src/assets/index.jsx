a5d58d9c7cdce324d565e60c7ef9d97cf01af64b
achmadrizky486 <achmadrizky486@gmail.com>
2022-07-28 17:39:21 +0700

    feat : login page

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,3 +1,4 @@
 import RegisterBg from "./image/bg.jpg";
+import LoginBg from "./image/LoginBg.jpg";
 
-export { RegisterBg };
+export { RegisterBg, LoginBg };
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { LoginBg } from "../../assets";
+import { Button, Gap, Input, Link } from "../../components";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex h-screen">
+      <div className="flex-1">
+        <img
+          src={LoginBg}
+          alt="image-register"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="w-[500px] p-8 flex flex-col justify-center">
+        <p className="text-xl font-bold mb-4 text-center">Login</p>
+        <Input label="Email" placeholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" type="password" />
+        <Gap height={20} />
+        <Button title="Login" />
+        <Gap height={100} />
+        <Link title="Daftar" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
