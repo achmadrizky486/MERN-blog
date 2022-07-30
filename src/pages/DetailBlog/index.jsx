73f2dff44b94d77315b0c224f7a5b043db6ea865
achmadrizky486 <achmadrizky486@gmail.com>
2022-07-30 13:16:07 +0700

    feat : detail blog page

--- src/pages/DetailBlog/index.jsx
+++ src/pages/DetailBlog/index.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { RegisterBg } from "../../assets";
 
 const DetailBlog = () => {
-  return <div>DetailBlog</div>;
+  return (
+    <div>
+      <img
+        className="w-full h-[300px] object-cover"
+        src={RegisterBg}
+        alt="img-thumb"
+      />
+      <p className="text-3xl font-bold mt-5 text-[#2d2d2d]">Title</p>
+      <p className="text-gray-500 text-sm">Author Date</p>
+      <p className="leading-9">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente
+        laborum error alias distinctio ipsa? Consequuntur minima dolore quos
+        earum eos eligendi nihil mollitia, officia architecto aut consequatur
+        odio, beatae corrupti.
+      </p>
+    </div>
+  );
 };
 
 export default DetailBlog;
