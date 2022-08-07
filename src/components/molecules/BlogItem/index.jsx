437eda62a9ca63532a547f040d09b5e56a40f44f
achmadrizky486 <achmadrizky486@gmail.com>
2022-08-07 16:36:28 +0700

    feat : dinamis data at home page

--- src/components/molecules/BlogItem/index.jsx
+++ src/components/molecules/BlogItem/index.jsx
@@ -3,21 +3,19 @@ import { RegisterBg } from "../../../assets";
 import { Link } from "../../atoms";
 import { useNavigate } from "react-router-dom";
 
-const BlogItem = () => {
+const BlogItem = (props) => {
+  const { image, title, name, date, body } = props;
   const history = useNavigate();
   return (
     <div>
-      <div className="w-full rounded-lg overflow-hidden shadow-lg shadow-black/40">
-        <img src={RegisterBg} alt="" className="w-full h-48 object-cover" />
+      <div className="w-full rounded-lg overflow-hidden shadow-lg shadow-black/40 hover:scale-105">
+        <img src={image} alt="" className="w-full h-48 object-cover" />
         <div className="p-4">
-          <p className="text-xl font-bold text-[#313131]">Title</p>
-          <p className="text-xs mt-3 text-gray-500">Author Date</p>
-          <p className="mt-4 text-base text-[#353535]">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere aut
-            perferendis praesentium architecto sed ut voluptatum corporis ullam
-            suscipit laudantium alias, quisquam animi possimus nulla quis
-            voluptate rerum excepturi pariatur.
+          <p className="text-xl font-bold text-[#313131] h-[50px]">{title}</p>
+          <p className="text-xs mt-3 text-gray-500">
+            {name} - {date}
           </p>
+          <p className="mt-4 text-base text-[#353535] h-[50px]">{body}</p>
           <div className="text-blue-500 w-fit">
             <Link title={"Read More"} onClick={() => history("/detail-blog")} />
           </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BlogItem } from "../../components";
 import { Button, Gap } from "../../components/atoms";
 import { useNavigate } from "react-router-dom";
 import Axios from "axios";
 
 const Home = () => {
+  const [dataBlog, setDataBlog] = useState([]);
   useEffect(() => {
     Axios.get("http://localhost:4000/v1/blog/posts")
       .then((result) => {
         console.log("result = ", result.data);
+        const response = result.data;
+        setDataBlog(response.data);
       })
       .catch((err) => {
         console.log("error = ", err);
@@ -22,10 +25,22 @@ const Home = () => {
         <Button title={"Create Post"} onClick={() => history("/create-blog")} />
       </div>
       <div className="grid grid-cols-2 w-full gap-10">
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
+        {dataBlog.map((blog) => {
+          return (
+            <BlogItem
+              key={blog._id}
+              image={`http://localhost:4000/${blog.image.replace(/\\/g, "/")}`}
+              title={blog.title.charAt(0).toUpperCase() + blog.title.slice(1)}
+              name={blog.author.name}
+              date={blog.createdAt}
+              body={
+                blog.body.substring(0, 100).charAt(0).toUpperCase() +
+                blog.body.slice(1).substring(0, 100) +
+                "..."
+              }
+            />
+          );
+        })}
       </div>
       <div className="mt-8 flex w-[400px] mx-auto">
         <Button title={"Prev"} />
